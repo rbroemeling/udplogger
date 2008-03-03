0ce8eb42e54524f209f9ed631bbec5b41029454c
Remi Broemeling <remi@nexopia.com>
2008-03-03 23:33:17 +0000

    * Rename targets -- they are log hosts from the clients' point of view.

--- trunk/serveradmin/udplogger/udploggercat.c
+++ trunk/serveradmin/udplogger/udploggercat.c
@@ -19,14 +19,15 @@ int arguments_parse(int, char **);
  */
 char *strndup(const char *s, size_t n);
 
+
 /*
- * Structure that is used to store information about the logging targets that
+ * Structure that is used to store information about the logging hosts that
  * the running instance of udploggercat is currently sending beacons to.  One host
  * per structure, arranged as a singly-linked list.
  */
-struct log_target_t {
+struct log_host_t {
 	struct sockaddr_in address;
-	struct log_target_t *next;
+	struct log_host_t *next;
 };
 
 
@@ -36,7 +37,7 @@ struct log_target_t {
  */
 struct udploggercat_configuration_t {
 	uintmax_t beacon_interval;
-	struct log_target_t target;
+	struct log_host_t log_host;
 } conf;
 
 
@@ -52,12 +53,11 @@ struct udploggercat_configuration_t {
  * main()
  *
  * Initializes the program configuration and state, then starts the beacon thread before
- * finally entering the main loop that reads stdin and outputs log entries to any hosts
- * on the target list.
+ * finally entering the main loop that reads log entries and dumps them to stdout.
  **/
 int main (int argc, char **argv)
 {
-	struct log_target_t *debug_target_ptr = &conf.target;
+	struct log_host_t *debug_log_host_ptr = &conf.log_host;
 	int result = 0;
 
 	result = arguments_parse(argc, argv);
@@ -68,10 +68,10 @@ int main (int argc, char **argv)
 
 #ifdef __DEBUG__
 	printf("udploggercat.c debug: parameter beacon_interval = '%lu'\n", conf.beacon_interval);
-	while (debug_target_ptr)
+	while (debug_log_host_ptr)
 	{
-		printf("udploggercat.c debug: logging target %s:%u\n", inet_ntoa(debug_target_ptr->address.sin_addr), debug_target_ptr->address.sin_port);
-		debug_target_ptr = debug_target_ptr->next;
+		printf("udploggercat.c debug: logging host %s:%u\n", inet_ntoa(debug_log_host_ptr->address.sin_addr), debug_log_host_ptr->address.sin_port);
+		debug_log_host_ptr = debug_log_host_ptr->next;
 	}
 #endif
 
@@ -89,24 +89,24 @@ int main (int argc, char **argv)
 int arguments_parse(int argc, char **argv)
 {
 	char *char_ptr;
-	struct hostent *host_tmp;
+	struct hostent *hostent_ptr;
 	char *hostname_tmp;
 	int i, j;
 	static struct option long_options[] =
 	{
 		{"help", no_argument, 0, 'h'},
+		{"host", required_argument, 0, 'o'},
 		{"interval", required_argument, 0, 'i'},
-		{"target", required_argument, 0, 't'},
 		{"version", no_argument, 0, 'v'},
 		{0, 0, 0, 0}
 	};
-	struct log_target_t *target_tmp;
+	struct log_host_t *log_host_ptr;
 	uintmax_t uint_tmp;
 	
 	/* Initialize our configuration to the default settings. */
 	conf.beacon_interval = DEFAULT_BEACON_INTERVAL;
-	memset(&conf.target, 0, sizeof(conf.target));
-	target_tmp = &conf.target;
+	memset(&conf.log_host, 0, sizeof(conf.log_host));
+	log_host_ptr = &conf.log_host;
 	
 	while (1)
 	{	
@@ -121,8 +121,8 @@ int arguments_parse(int argc, char **argv)
 				printf("Usage: udploggercat [OPTIONS]\n");
 				printf("\n");
 				printf("  -h, --help                 display this help and exit\n");
+				printf("  -o, --host <host>:<port>   host and port to target with beacon transmissions (default broadcast)\n");
 				printf("  -i, --interval <interval>  interval in seconds between beacon transmissions (default %lu)\n", DEFAULT_BEACON_INTERVAL);
-				printf("  -t, --target <host>:<port> host and port to target with beacon transmissions (default broadcast)\n");
 				printf("  -v, --version              display udploggercat version and exit\n");          
 				printf("\n");
 				return 0;
@@ -135,18 +135,18 @@ int arguments_parse(int argc, char **argv)
 				}
 				conf.beacon_interval = uint_tmp;
 				break;
-			case 't':
+			case 'o':
 				char_ptr = strstr(optarg, ":");
 				if (! char_ptr || (char_ptr == optarg))
 				{
-					fprintf(stderr, "udploggercat.c invalid target specification '%s'\n", optarg);
+					fprintf(stderr, "udploggercat.c invalid host specification '%s'\n", optarg);
 					return -1;				
 				}
 				
 				hostname_tmp = strndup(optarg, (char_ptr - optarg));
 				if (! hostname_tmp)
 				{
-					fprintf(stderr, "udploggercat.c could not allocate memory to record target host '%s'\n", optarg);
+					fprintf(stderr, "udploggercat.c could not allocate memory to record host '%s'\n", optarg);
 					return -1;
 				}
 				
@@ -156,43 +156,43 @@ int arguments_parse(int argc, char **argv)
 					uint_tmp = strtoumax(char_ptr, 0, 10);
 					if (! uint_tmp || uint_tmp == UINT_MAX)
 					{
-						fprintf(stderr, "udploggercat.c invalid port in target specification '%s'\n", optarg);
+						fprintf(stderr, "udploggercat.c invalid port in host specification '%s'\n", optarg);
 						free(hostname_tmp);
 						return -1;
 					}
 				}
 				else
 				{
-					fprintf(stderr, "udploggercat.c missing port in target specification '%s'\n", optarg);
+					fprintf(stderr, "udploggercat.c missing port in host specification '%s'\n", optarg);
 					free(hostname_tmp);
 					return -1;
 				}		
 
-				host_tmp = gethostbyname(hostname_tmp);
-				if (! host_tmp)
+				hostent_ptr = gethostbyname(hostname_tmp);
+				if (! hostent_ptr)
 				{
-					fprintf(stderr, "udploggercat.c could not find the IP address of the target host '%s'\n", hostname_tmp);
+					fprintf(stderr, "udploggercat.c could not find the IP address of the host '%s'\n", hostname_tmp);
 					free(hostname_tmp);
 					return -1;
 				}
 				free(hostname_tmp);
 				
-				for (j = 0; host_tmp->h_addr_list[j] != NULL; j++)
+				for (j = 0; hostent_ptr->h_addr_list[j] != NULL; j++)
 				{
-					if (target_tmp->address.sin_family == AF_INET)
+					if (log_host_ptr->address.sin_family == AF_INET)
 					{
-						target_tmp->next = calloc(1, sizeof(struct log_target_t));
-						if (! target_tmp->next)
+						log_host_ptr->next = calloc(1, sizeof(struct log_host_t));
+						if (! log_host_ptr->next)
 						{
 							perror("udploggercat.c calloc()");
 							return -1;
 						}
-						target_tmp = target_tmp->next;
+						log_host_ptr = log_host_ptr->next;
 					}
 					
-					target_tmp->address.sin_family = AF_INET;
-					target_tmp->address.sin_addr.s_addr = ((struct in_addr *)(host_tmp->h_addr_list[j]))->s_addr;
-					target_tmp->address.sin_port = uint_tmp;
+					log_host_ptr->address.sin_family = AF_INET;
+					log_host_ptr->address.sin_addr.s_addr = ((struct in_addr *)(hostent_ptr->h_addr_list[j]))->s_addr;
+					log_host_ptr->address.sin_port = uint_tmp;
 				}
 				break;
 			case 'v':
