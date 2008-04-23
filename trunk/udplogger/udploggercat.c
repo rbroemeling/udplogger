600648a228e95f812f247b1726ebeb07cdcddf92
Remi Broemeling <remi@nexopia.com>
2008-04-23 17:19:35 +0000

    * Update udploggercat to replace the delimiter with a custom delimiter for udploggercat.    This delimiter will be set through a command-line parameter and will default to ' ' (a space).

--- trunk/udplogger/udploggercat.c
+++ trunk/udplogger/udploggercat.c
@@ -1,6 +1,24 @@
 #include <arpa/inet.h>
 #include <stdio.h>
+#include <string.h>
 #include <time.h>
+#include "udplogger.h"
+
+
+/*
+ * Give a strnlen prototype to stop compiler warnings.  GNU libraries give it to us,
+ * but ISO C90 doesn't allow us to use _GNU_SOURCE before including <string.h>.
+ */
+size_t strnlen(const char *, size_t);
+
+
+/*
+ * Structure that contains configuration information for the running instance
+ * of this udploggercat client program.
+ */
+struct udploggercat_configuration_t {
+	unsigned char delimiter_character;
+} udploggercat_conf;
 
 
 /*
@@ -16,11 +34,12 @@
  * Trivial implementation of log_packet_hook for linking with udploggerclientlib.o.
  * Prints a timestamp, the source host information, and then the log data itself.
  **/
-void inline log_packet_hook(const struct sockaddr_in *sender, const char *line)
+void inline log_packet_hook(struct sockaddr_in *sender, char *line)
 {
 	static time_t current_timestamp = 0;
 	static struct tm current_time;
 	static char current_time_str[TIME_STRING_BUFFER_SIZE];
+	int i;
 
 	if ((! current_timestamp) || (current_timestamp != time(NULL)))
 	{
@@ -38,5 +57,15 @@ void inline log_packet_hook(const struct sockaddr_in *sender, const char *line)
 		strftime(current_time_str, TIME_STRING_BUFFER_SIZE, "[%Y-%m-%d %H:%M:%S]", &current_time);
 		current_time_str[TIME_STRING_BUFFER_SIZE - 1] = '\0';
 	}
-	printf("%s [%s:%hu] %s\n", current_time_str, inet_ntoa(sender->sin_addr), ntohs(sender->sin_port), line);
+	if (udploggercat_conf.delimiter_character != DELIMITER_CHARACTER)
+	{
+		for (i = 0; i < strnlen(line, PACKET_MAXIMUM_SIZE); i++)
+		{
+			if (line[i] == DELIMITER_CHARACTER)
+			{
+				line[i] = udploggercat_conf.delimiter_character;
+			}
+		}
+	}
+	printf("%s%c[%s:%hu]%c%s\n", current_time_str, udploggercat_conf.delimiter_character, inet_ntoa(sender->sin_addr), ntohs(sender->sin_port), udploggercat_conf.delimiter_character, line);
 }
